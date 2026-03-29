6f2e03cea0bcef3f975ab614f655b382f379abaf
Harsh Makadiya <harshharsh312@gmail.com>
2026-03-29 20:34:04 +0530

    fix build

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  url: process.env.NEXT_PUBLIC_SITE_URL,
+  url: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
   name: "Harsh Makadiya",
   title: "Harsh Makadiya — Software Development Engineer",
   description:
